752088ac10ac7d2b9134732d355da79a25779080
Gustave Habimana <ghabiman@gmail.com>
2020-02-15 00:16:40 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -44,6 +44,8 @@ def apply_coupons(cart, coupons)
     coupons.each do |coupon|
     item_info = find_item_by_name_in_collection(coupon[:item], cart)
     item_w_coupon = find_item_by_name_in_collection(coupon[:item]+" W/COUPON", cart)
+    if item_w_coupon and item_info[:count] >= coupon[:num]
+
 
 
 
