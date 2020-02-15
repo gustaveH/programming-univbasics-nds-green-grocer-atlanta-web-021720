39b790dcc802fb61200756b7f6aba3de75894835
Gustave Habimana <ghabiman@gmail.com>
2020-02-15 00:09:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -4,6 +4,8 @@ def find_item_by_name_in_collection(name, collection)
   # Consult README for inputs and outputs
   
     collection.each do |item_info|
+          if item_info[:item] == name
+
 
 end
 
