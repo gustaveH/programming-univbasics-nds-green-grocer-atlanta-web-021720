8bcb7ea9759adae9414abf8de50a82d87a43bd89
Gustave Habimana <ghabiman@gmail.com>
2020-02-15 01:41:00 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -30,9 +30,7 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  
 end
 
 def apply_clearance(cart)
